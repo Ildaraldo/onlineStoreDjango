be7f4978247d378a8ffb85583263c693cb24edc0
programmer <ildarinho99@gmail.com>
2024-07-02 00:44:42 +0500

    Доработка filling.py

--- catalog/management/commands/filling.py
+++ catalog/management/commands/filling.py
@@ -5,6 +5,23 @@ from catalog.models import Product, Category
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        # создаём категории
+        category_list = [
+            {"name": "Игрушки", "description": "Игрушки для детей", "is_active": 1},
+            {"name": "Товары для животных", "description": "Товары для всех животных", "is_active": 1},
+            {"name": "Соки", "description": "Натуральные соки", "is_active": 1}
+        ]
+
+        category_for_create = []
+        for category_item in category_list:
+            category_for_create.append(Category(**category_item))
+
+        # удаляем старые записи
+        Category.objects.all().delete()
+
+        # добавляем новые записи
+        Category.objects.bulk_create(category_for_create)
+
         # загружаем все категории
         categories = Category.objects.all()
 
